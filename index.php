680d1c2cf2608ac6c566036c066f33db774ecf19
Maicol D. Mamani Chalco <mdmch13@gmail.com>
2026-05-12 23:04:16 -0300

    Archivo principal del sistema de inventario

--- index.php
+++ index.php
@@ -1,3 +1,5 @@
+<!--INICIO DE SESION-->
+<?php include "./inc/session_start.php"; ?>
 <!DOCTYPE html>
 <html lang="es">
 <head>
@@ -7,10 +9,25 @@
 <body>
     <!--NAVEGADOR-->
     <?php
-        include "./inc/navbar.php";
-    ?>
+        if(!isset($_GET['vista']) || $_GET['vista'] == ""){
+            $_GET['vista'] = "login";
+        }
 
-    <!--JAVASCRIPT-->
-    <?php include "./inc/script.php"; ?>
+        if(is_file("./vistas/".$_GET['vista'].".php") && $_GET['vista'] != "login"
+        && $_GET['vista'] != "404"){
+            include "./inc/navbar.php";
+
+            include "./vistas/".$_GET['vista'].".php";
+
+            //JAVASCRIPT BULMA
+            include "./inc/script.php"; 
+        }else{
+            if($_GET['vista'] == "login"){
+                include "./vistas/login.php";
+            }else{
+                include "./vistas/404.php";
+            }
+        }
+    ?>
 </body>
 </html>
