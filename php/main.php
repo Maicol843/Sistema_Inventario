c3f053bb0649d483ca562c73f73f8bbf0f4b6a2e
Maicol D. Mamani Chalco <mdmch13@gmail.com>
2026-05-12 23:50:01 -0300

    Función para validar formularios con expresiones regulares

--- php/main.php
+++ php/main.php
@@ -1,9 +1,20 @@
 <?php
 
-    # Conexión a la Base de Datos
+    # Conexión a la Base de Datos #
     function conexion(){
         $pdo = new PDO('mysql:host=localhost; dbname=inventario', 'root', '');
         return $pdo;
     }
+
+    # Verificar datos #
+    function verificar_datos($filtro,$cadena){
+        if(preg_match("/^".$filtro."$/",$cadena)){
+            return false;
+        }else{
+            return true;
+        }
+    }
+
+    
 
     
