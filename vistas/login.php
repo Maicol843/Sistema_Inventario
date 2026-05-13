6afc8324f8e78aee6a146c331c2b46e4c8d3a0d3
Maicol D. Mamani Chalco <mdmch13@gmail.com>
2026-05-12 23:32:45 -0300

    Creando vistas
    Se creó las vistas de login, home y pagina 404

--- vistas/login.php
+++ vistas/login.php
@@ -1,1 +1,27 @@
-LOGIN
+<div class="main-container">
+
+    <!--FORMULARIO-->
+    <form class="box login" action="" method="POST" autocomplete="off">
+        <h5 class="tittle is-5 has-text-centered is-uppercase">Sistema de Inventario</h5>
+
+        <div class="field">
+
+            <label class="label">Usuario</label>
+
+            <div class="control">
+                <input type="text" class="input" name="login_usuario" pattern="[a-zA-Z0-9] {4,20}" maxlength="20" required>
+            </div>
+
+            <div class="field">
+                <label class="label">Clave</label>
+                <div class="control">
+                    <input type="password" class="input" name="login_clave" pattern="[a-zA-Z0-9$@.-] {7,100}" maxlength="100" required>
+                </div>
+                <p class="has-text-centered mb-4 mt-3">
+                    <button type="submit" class="button is-info">Iniciar Sesión</button>
+                </p>
+            </div>
+        </div>
+    </form>
+
+</div>
