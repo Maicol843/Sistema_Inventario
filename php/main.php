7307985581cfb738ab32661409be8ab33938c2ce
Maicol D. Mamani Chalco <mdmch13@gmail.com>
2026-05-14 13:04:46 -0300

    Función PHP para renombrar fotos

--- php/main.php
+++ php/main.php
@@ -46,6 +46,18 @@
 		return $cadena;
 	}
 
+	# Función renombrar fotos #
+	function renombrar_fotos($nombre){
+		$nombre = str_ireplace(" ", "_",$nombre);
+		$nombre = str_ireplace("/", "_",$nombre);
+		$nombre = str_ireplace("#", "_",$nombre);
+		$nombre = str_ireplace("-", "_",$nombre);
+		$nombre = str_ireplace("$", "_",$nombre);
+		$nombre = str_ireplace(".", "_",$nombre);
+		$nombre = str_ireplace(",", "_",$nombre);
+		$nombre = $nombre."_".rand(0,100);
+		return $nombre;
+	}
     
 
     
