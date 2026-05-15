f72f329c47c853b6fe06d812de58e43dd14df3c5
Maicol D. Mamani Chalco <mdmch13@gmail.com>
2026-05-15 13:32:24 -0300

    Se creó el sistema de inicio de sesión (login) usando PHP y MySQL

--- vistas/login.php
+++ vistas/login.php
@@ -1,27 +1,33 @@
 <div class="main-container">
 
-    <!--FORMULARIO-->
-    <form class="box login" action="" method="POST" autocomplete="off">
-        <h5 class="tittle is-5 has-text-centered is-uppercase">Sistema de Inventario</h5>
+	<form class="box login" action="" method="POST" autocomplete="off">
+		<h5 class="title is-5 has-text-centered is-uppercase">Sistema de inventario</h5>
 
-        <div class="field">
+		<div class="field">
+			<label class="label">Usuario</label>
+			<div class="control">
+			    <input class="input" type="text" name="login_usuario" pattern="[a-zA-Z0-9]{4,20}" maxlength="20" required >
+			</div>
+		</div>
 
-            <label class="label">Usuario</label>
+		<div class="field">
+		  	<label class="label">Clave</label>
+		  	<div class="control">
+		    	<input class="input" type="password" name="login_clave" pattern="[a-zA-Z0-9$@.-]{7,100}" maxlength="100" required >
+		  	</div>
+		</div>
 
-            <div class="control">
-                <input type="text" class="input" name="login_usuario" pattern="[a-zA-Z0-9] {4,20}" maxlength="20" required>
-            </div>
+		<p class="has-text-centered mb-4 mt-3">
+			<button type="submit" class="button is-info is-rounded">Iniciar sesion</button>
+		</p>
 
-            <div class="field">
-                <label class="label">Clave</label>
-                <div class="control">
-                    <input type="password" class="input" name="login_clave" pattern="[a-zA-Z0-9$@.-] {7,100}" maxlength="100" required>
-                </div>
-                <p class="has-text-centered mb-4 mt-3">
-                    <button type="submit" class="button is-info">Iniciar Sesión</button>
-                </p>
-            </div>
-        </div>
-    </form>
+		<?php
+			if(isset($_POST['login_usuario']) && isset($_POST['login_clave'])){
+				require_once "./php/main.php";
+				require_once "./php/iniciar_sesion.php";
+			}
+		?>
+	</form>
+
 
 </div>
